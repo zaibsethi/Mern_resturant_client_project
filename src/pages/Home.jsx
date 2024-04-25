b964207bc0afb034970e77e29b9a86a2ec2a9dda
jahanzaib_007 <zaibsethi@gmail.com>
2024-04-25 18:13:24 +0200

    quality section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import About from '../components/About'
+import Qualities from '../components/Qualities'
 
 function Home() {
   return (
     <>
     <HeroSection/>
     <About/>
+    <Qualities/>
     </>
   )
 }
