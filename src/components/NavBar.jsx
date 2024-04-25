fc61a45c20d930142928f49fc2e69fa6bfe39a5d
jahanzaib_007 <zaibsethi@gmail.com>
2024-04-25 02:18:25 +0200

    dynamic Nav & Hero secion

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import {Link} from 'react-scroll'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {data} from '../restApi.json'
+
+function NavBar() {
+    const [show, setShow] =useState(false)
+  return (
+  <nav>
+<div className="logo">FOOD</div>
+
+<div className={show ? "navLinks showmenu": "navLinks"}>
+  <div className="links">
+
+    {data[0].navbarLinks.map((element)=>{
+return (
+    <Link  to={element.link} key={element.id} spy={true} smooth={true} duration={500} >
+        {element.title}
+    </Link>
+)
+
+ 
+    }
+
+
+
+    )}
+  </div>
+<button className='menuBtn'>OUR MENU</button>
+
+</div>
+<div className="hamburger" onClick={()=>{setShow(!show)}}>
+<GiHamburgerMenu/>
+
+</div>
+  </nav>
+  )
+}
+
+export default NavBar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <HeroSection/>
+    </>
   )
 }
 
